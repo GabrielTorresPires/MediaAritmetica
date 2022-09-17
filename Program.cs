335fa026cdf96b7456afde91ae551adb6d10a94c
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-16 22:43:00 -0300

    solução Media Aritmetica

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Descubra a Média Aritmética de 3 números");
+
+Console.WriteLine("Digite o primeiro número: ");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+double numero3 = Convert.ToDouble(Console.ReadLine());
+
+double media = (numero1+numero2+numero3) / 3;
+
+Console.WriteLine($"\n A Média é: {media:N1}");
+
